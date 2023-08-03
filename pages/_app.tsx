1932e5c77ae0d7155dfe37fc3af03d0391ff6b8f
David <naranjo.david.1999@gmail.com>
2023-08-03 01:05:58 -0500

    Nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/system";
 
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
