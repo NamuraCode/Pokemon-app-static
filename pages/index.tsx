fe5a511eb1efcd0c0dc2f9d8077b300f903c179a
David <naranjo.david.1999@gmail.com>
2023-08-03 01:16:20 -0500

    button nextui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
     <main>
-      <h1>Next.js + TypeScript</h1>
+      <h1 className="">Next.js + TypeScript</h1>
+      <Button color="danger">Click me</Button>
     </main>
   );
 }
