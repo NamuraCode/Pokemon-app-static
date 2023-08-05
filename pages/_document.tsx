b081cc961f6c51dc239e78bbe185d26eb90fd59a
David <naranjo.david.1999@gmail.com>
2023-08-04 23:53:36 -0500

    _document config to nextui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,21 +11,23 @@ class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const originalRenderPage = ctx.renderPage;
+    // ctx has a response object response, manipulate the response object.
+  // Or ') {'
+    // configuration recommended by nextjs
+    // const originalRenderPage = ctx.renderPage;
 
     // Run the React rendering logic synchronously
-    ctx.renderPage = () =>
-      originalRenderPage({
+    // ctx.renderPage = () =>
+    //   originalRenderPage({
         // Useful for wrapping the whole react tree
-        enhanceApp: (App) => App,
+        // enhanceApp: (App) => App,
         // Useful for wrapping in a per-page basis
-        enhanceComponent: (Component) => Component,
-      });
+        // enhanceComponent: (Component) => Component,
+      // });
 
     // Run the parent `getInitialProps`, it now includes the custom `renderPage`
     const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
+    return { ...initialProps};
   }
 
   render() {
