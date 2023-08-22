dda10c03b5c03c4c7302d15baad10e93bc16f887
David <naranjo.david.1999@gmail.com>
2023-08-21 23:30:05 -0500

    separate pokemon card of home

---
+++ components/pokemon/PokemonCard.tsx
@@ -0,0 +1,35 @@
+import { SmallPokemon } from "@/interfaces";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import Link from "next/link";
+
+interface Props {
+  pokemon: SmallPokemon;
+}
+export const PokemonCart = ({ pokemon }: Props) => {
+  return (
+    <Card
+      shadow="sm"
+      key={pokemon.id}
+      isPressable
+      onPress={() => console.log("item pressed")}
+    >
+      <CardBody className="overflow-visible p-0">
+        <Image
+          shadow="sm"
+          radius="lg"
+          width="100%"
+          alt={pokemon.name}
+          className="w-full object-contain h-[140px]"
+          src={pokemon.img}
+        />
+      </CardBody>
+      <CardFooter className="text-small justify-between">
+        <b className="text-default-500">{pokemon.name}</b>
+        <p className="text-default-500">{pokemon.id}</p>
+        <Link href={pokemon.url} target="blank" className="text-default-500">
+          go to {pokemon.name}
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { pokemonApi } from "@/api";
 import { PokemonListRes, SmallPokemon } from "@/interfaces";
 import Link from "next/link";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { PokemonCart } from "@/components/pokemon/PokemonCard";
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -15,33 +16,8 @@ const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Listado de pokemons">
       <div className="max-w-[100%] gap-6 grid grid-cols-4 grid-rows-2">
-        {pokemons.map(({ id, name, img, url }, index) => {
-          return (
-            <Card
-              shadow="sm"
-              key={index}
-              isPressable
-              onPress={() => console.log("item pressed")}
-            >
-              <CardBody className="overflow-visible p-0">
-                <Image
-                  shadow="sm"
-                  radius="lg"
-                  width="100%"
-                  alt={name}
-                  className="w-full object-cover h-[140px]"
-                  src={img}
-                />
-              </CardBody>
-              <CardFooter className="text-small justify-between">
-                <b>{name}</b>
-                <p className="text-default-500">{id}</p>
-                <Link href={url} target="blank" className="text-default-500">
-                  go to {name}
-                </Link>
-              </CardFooter>
-            </Card>
-          );
+        {pokemons.map((pokemon) => {
+          return <PokemonCart pokemon={pokemon} />;
         })}
       </div>
     </Layout>
