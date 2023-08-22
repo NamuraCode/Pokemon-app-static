69551a018449d7d7aecebbabbde3791700fb8dd3
David <naranjo.david.1999@gmail.com>
2023-08-21 23:08:07 -0500

    cards nextui test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { GetStaticProps, NextPage } from "next";
 import { Layout } from "@/components/layouts";
 import { pokemonApi } from "@/api";
 import { PokemonListRes, SmallPokemon } from "@/interfaces";
-import Image from "next/image";
 import Link from "next/link";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -14,26 +14,36 @@ const Home: NextPage<Props> = ({ pokemons }) => {
   console.log(pokemons);
   return (
     <Layout title="Listado de pokemons">
-      <ul>
-        {pokemons.map((pokemon, index) => {
+      <div className="max-w-[100%] gap-6 grid grid-cols-4 grid-rows-2">
+        {pokemons.map(({ id, name, img, url }, index) => {
           return (
-            <li key={index}>
-              <span>id: {pokemon.id}</span>
-              <br />
-              <span>name: {pokemon.name}</span>
-              <Image
-                src={pokemon.img}
-                alt={pokemon.name}
-                width={100}
-                height={100}
-              />
-              <Link href={pokemon.url} target="blank">
-                go to {pokemon.name}
-              </Link>
-            </li>
+            <Card
+              shadow="sm"
+              key={index}
+              isPressable
+              onPress={() => console.log("item pressed")}
+            >
+              <CardBody className="overflow-visible p-0">
+                <Image
+                  shadow="sm"
+                  radius="lg"
+                  width="100%"
+                  alt={name}
+                  className="w-full object-cover h-[140px]"
+                  src={img}
+                />
+              </CardBody>
+              <CardFooter className="text-small justify-between">
+                <b>{name}</b>
+                <p className="text-default-500">{id}</p>
+                <Link href={url} target="blank" className="text-default-500">
+                  go to {name}
+                </Link>
+              </CardFooter>
+            </Card>
           );
         })}
-      </ul>
+      </div>
     </Layout>
   );
 };
@@ -43,7 +53,9 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => ({
     ...pokemon,
     id: index + 1,
-    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
   }));
 
   return {
