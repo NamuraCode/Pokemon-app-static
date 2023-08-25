097656f1a19b950f4c96b58cf4cad27442882153
David <naranjo.david.1999@gmail.com>
2023-08-25 00:40:50 -0500

    static pokemons pages by id

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,18 +1,21 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 
 import { Layout } from "@/components/layouts";
+import { pokemonApi } from "@/api";
+import { PokemonData } from "@/interfaces";
+import { Image } from "@nextui-org/react";
 
 interface Props {
-  id: number;
-  name: string;
+  pokemon: PokemonData;
 }
 
-const PokemonPage = ({ id, name }: Props) => {
+const PokemonPage = ({ pokemon }: Props) => {
   return (
-    <Layout title={`Pokemon - ${name}`}>
+    <Layout title={`Pokemon - ${pokemon.id}`}>
       <h1>
-        {id} - {name}
+        {pokemon.id} - {pokemon.name}
       </h1>
+      <Image src={`${pokemon.sprites.front_default}`}></Image>
     </Layout>
   );
 };
@@ -20,9 +23,7 @@ const PokemonPage = ({ id, name }: Props) => {
 export default PokemonPage;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const numberPokemons = [...Array(151)].map((value, index)=>`${index +1 }`)
-  console.log(numberPokemons);
-  
+  const numberPokemons = [...Array(151)].map((value, index) => `${index + 1}`);
   return {
     paths: numberPokemons.map((id) => ({
       params: { id },
@@ -31,13 +32,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  //   const { data } = await pokemonApi.get<PokemonListRes>("/pokemon?limit=151");
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+  const { data } = await pokemonApi.get<PokemonData>(`/pokemon/${id}`);
   return {
     props: {
-      id: 1,
-      name: "Bulbasaur",
+      pokemon: data,
     },
   };
 };
