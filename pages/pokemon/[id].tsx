b24377d539286bf187a039a69dec8eb2509166a7
David <naranjo.david.1999@gmail.com>
2023-08-26 00:44:40 -0500

    cart pokemon

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -3,7 +3,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { Layout } from "@/components/layouts";
 import { pokemonApi } from "@/api";
 import { PokemonData } from "@/interfaces";
-import { Card, CardBody, Image } from "@nextui-org/react";
+import { Button, Card, CardBody, Image, Spacer } from "@nextui-org/react";
 
 interface Props {
   pokemon: PokemonData;
@@ -12,9 +12,54 @@ interface Props {
 const PokemonPage = ({ pokemon }: Props) => {
   return (
     <Layout title={`Pokemon - ${pokemon.id}`}>
-      <Card className="py-4">
-        <CardBody className="flex overflow-visible py-2">
-          <Image src={`${pokemon.sprites.front_default}`}/>
+      <Card className="m-auto bg-transparent max-w-3xl mt-10 flex flex-row">
+        <CardBody className="flex justify-center align-middle bg-background2">
+          <Image
+            alt={pokemon.name}
+            className={"self-center"}
+            src={`${pokemon.sprites.front_default}`}
+            width={300}
+          />
+        </CardBody>
+        <Spacer x={4} />
+        <CardBody className="flex justify-between align-middle bg-background2 py-unit-1xl">
+          <div className="flex flex-row justify-between align-bottom">
+            <h2 className="flex items-center text-2xl capitalize">{pokemon.name}</h2>
+            <Button color={"primary"}>Save favorites</Button>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-tiny text-white/80">Sprites:</p>
+            <div className="flex flex-row">
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.front_default}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.back_default}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.front_shiny}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.back_shiny}`}
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
         </CardBody>
       </Card>
     </Layout>
