fb9f69f6454c64128684104223338b9b40546e5e
David <naranjo.david.1999@gmail.com>
2023-09-05 23:01:29 -0500

    params name path

---
+++ pages/name/[name].tsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Button, Card, CardBody, Image, Spacer } from "@nextui-org/react";
+
+import confetti from "canvas-confetti";
+
+import { pokemonApi } from "@/api";
+import { PokemonData, PokemonListRes, SmallPokemon } from "@/interfaces";
+import { Layout } from "@/components/layouts";
+import favoritesController from "@/utils/favorites.controller";
+
+interface Props {
+  pokemon: PokemonData;
+}
+
+const PokemonPage = ({ pokemon }: Props) => {
+  const [isInFavorites, setIsInFavorites] = useState(false);
+
+  useEffect(() => {
+    setIsInFavorites(favoritesController.isInFavorites(pokemon.id));
+  }, []);
+
+  const toggleSaveFavorites = () => {
+    favoritesController.saveInFavorites(pokemon.id);
+    setIsInFavorites(!isInFavorites);
+
+    if (isInFavorites) return;
+
+    confetti({
+      zIndex: 99,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0,
+      },
+    });
+  };
+
+  return (
+    <Layout title={`Pokemon - ${pokemon.name}`}>
+      <Card className="m-auto bg-transparent max-w-3xl mt-10 flex flex-row">
+        <CardBody className="flex justify-center items-center bg-background2">
+          <Image
+            alt={pokemon.name}
+            className={"self-center"}
+            src={`${pokemon.sprites.front_default}`}
+            width={300}
+          />
+        </CardBody>
+        <Spacer x={4} />
+        <CardBody className="flex justify-between bg-background2">
+          <div className="flex flex-row justify-between align-bottom">
+            <h2 className="flex items-center text-2xl capitalize">
+              {pokemon.name}
+            </h2>
+            <Button
+              color={isInFavorites ? "primary" : "success"}
+              onClick={toggleSaveFavorites}
+            >
+              {isInFavorites ? "Delete favorites" : "Save favorites"}
+            </Button>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-tiny text-white/80">Sprites:</p>
+            <div className="flex flex-row">
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.front_default}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.back_default}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.front_shiny}`}
+                width={100}
+                height={100}
+              />
+              <Image
+                alt={pokemon.name}
+                className={"self-center"}
+                src={`${pokemon.sprites.back_shiny}`}
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </Layout>
+  );
+};
+
+export default PokemonPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await pokemonApi.get<PokemonListRes>("/pokemon?limit=151");
+  const pokemons: SmallPokemon[] = data.results;
+  
+  return {
+    paths: pokemons.map(({ name }) => ({
+      params: { name },
+    })),
+    fallback: false, // false or "blocking"
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { name } = params as { name: string };
+  const { data } = await pokemonApi.get<PokemonData>(`/pokemon/${name}`);
+  return {
+    props: {
+      pokemon: data,
+    },
+  };
+};
