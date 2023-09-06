97fa49ba30d23f8b4394d1f7bc03bd6bbb49dd07
David <naranjo.david.1999@gmail.com>
2023-09-05 23:14:42 -0500

    favorite key

--- components/ui/FavoritesList.tsx
+++ components/ui/FavoritesList.tsx
@@ -7,7 +7,7 @@ export const FavoritesList = ({ pokemons }: Props) => {
   return (
     <div className="max-w-[100%] gap-6 grid grid-cols-4 grid-rows-2">
       {pokemons.map((id) => {
-        return <FavoritesCard id={id}/>;
+        return <FavoritesCard key={id} id={id}/>;
       })}
     </div>
   );
