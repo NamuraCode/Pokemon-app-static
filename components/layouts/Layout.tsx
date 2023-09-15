e6c010377bb08423e52b0fc75843ae0f0727ab54
David <naranjo.david.1999@gmail.com>
2023-09-14 23:49:59 -0500

    pretty

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,30 +1,40 @@
-import Head from "next/head";
-import { Navbar } from "../ui";
+import Head from 'next/head';
+import { Navbar } from '../ui';
 
 type LayoutProps = {
   children: React.ReactNode;
   title?: string;
 };
 
-const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export const Layout = ({ children, title }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>{title || "PokemonApp"}</title>
-        <meta name="author" content="David Naranjo" />
-        <meta name="description" content={`Information about pokemon ${title}`}/>
-        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <title>{title || 'PokemonApp'}</title>
+        <meta name='author' content='David Naranjo' />
+        <meta
+          name='description'
+          content={`Information about pokemon ${title}`}
+        />
+        <meta name='keywords' content={`${title}, pokemon, pokedex`} />
 
-        <meta property="og:title" content={`Info about pokemon: ${title}`} />
-        <meta property="og:description" content={`This page is about ${title}`} />
-        <meta property="og:image" content={`${origin}/images/banner.png`} />
+        <meta property='og:title' content={`Info about pokemon: ${title}`} />
+        <meta
+          property='og:description'
+          content={`This page is about ${title}`}
+        />
+        <meta property='og:image' content={`${origin}/images/banner.png`} />
       </Head>
-      <Navbar/>
-      <main style={{
-        padding: '0px 20px'
-      }}>{children}</main>
+      <Navbar />
+      <main
+        style={{
+          padding: '0px 20px',
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
